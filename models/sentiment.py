7b4bb7c05b334e5056b9315b4d6e2ed8526d4608
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-30 15:20:02 -0400

    Add fields to Sentiment model
    Added id, user_id, user_text_input, and sentiment_label fields to the Sentiment SQLAlchemy model.

--- models/sentiment.py
+++ models/sentiment.py
@@ -6,3 +6,8 @@ base = declarative_base()
 
 class Sentiment(base):
     __tablename__ = "sentiments"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = (Integer)
+    user_text_input = Column(String)
+    sentiment_label = Column(String)
