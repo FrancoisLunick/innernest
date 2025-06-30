bd87d29895b31a3d3ca189163a3d19e6586a3226
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-30 15:15:50 -0400

    Add Sentiment model to sentiment.py
    Introduces the Sentiment class as a SQLAlchemy model with the table name 'sentiments'.

--- models/sentiment.py
+++ models/sentiment.py
@@ -1,2 +1,8 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+
+# Base class for the model
+base = declarative_base()
+
+class Sentiment(base):
+    __tablename__ = "sentiments"
