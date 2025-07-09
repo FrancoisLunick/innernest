2341881674332ac7f300a1d1344b684e4899b82e
Lunick Francois <LunickFrancois22@gmail.com>
2025-07-09 19:05:30 -0400

    Fix model loading in SentimentAnalyzer
    Corrects the model path usage in SentimentAnalyzer by storing it as an instance variable and passing it to joblib.load.

--- ml/sentiment_analysis.py
+++ ml/sentiment_analysis.py
@@ -4,9 +4,10 @@ import os
 class SentimentAnalyzer:
     def __init__(self):
         base_dir = os.path.dirname(__file__)
-        model_path = os.path.join(base_dir, "embeddings", "tfidf_model.pk1")
+        self.model_path = os.path.join(base_dir, "embeddings", "tfidf_model.pk1")
         self.model = None
     
     def load_sentiment_model(self):
         if not self.model:
-            self.model = joblib.load()
+            self.model = joblib.load(self.model_path)
+        return self.model
