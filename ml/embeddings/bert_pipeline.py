5e2d1de81b870ed114a45e45ee04badcb2563fe7
Lunick Francois <LunickFrancois22@gmail.com>
2025-07-11 21:20:36 -0400

    Add load_data function to bert_pipeline
    Introduces a load_data function that extracts texts and labels from training_data, and returns them as a tuple.

--- ml/embeddings/bert_pipeline.py
+++ ml/embeddings/bert_pipeline.py
@@ -5,3 +5,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 from ml.ml_data.training_data import training_data
 
 bert_tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+
+def load_data():
+    texts = [entry[0] for entry in training_data]
+    labels = [entry[1] for entry in training_data]
+    
+    print([(texts, labels)])
+    
+    return (texts, labels)
