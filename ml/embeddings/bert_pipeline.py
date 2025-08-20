e043506026c3ff111f82ee872b2bed2ebe42c5b4
Lunick Francois <LunickFrancois22@gmail.com>
2025-08-19 21:21:06 -0400

    Add training function for BERTClassifier pipeline
    Wrote the train_model function to run the training loop for the BERT classifier.

--- ml/embeddings/bert_pipeline.py
+++ ml/embeddings/bert_pipeline.py
@@ -57,3 +57,24 @@ class BERTClassifier(torch.nn.Module):
         logits = self.classifier(x)
         
         return logits
+
+def train_model(model, dataloader, optimizer, criterion, device):
+    
+    model.train()
+    total_loss = 0.0
+    
+    for batch in dataloader:
+        
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        loss = criterion(outputs, labels.float())
+        loss.backward()
+        optimizer.step()
+        
+        total_loss += loss.item()
+        
+        return total_loss / len(dataloader)
