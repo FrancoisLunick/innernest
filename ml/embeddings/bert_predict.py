e74b10bb760a95619a9f1ebc7316117d81073c75
Lunick Francois <LunickFrancois22@gmail.com>
2026-01-29 13:03:54 -0500

    Add BERT inference pipeline for mood prediction

--- ml/embeddings/bert_predict.py
+++ ml/embeddings/bert_predict.py
@@ -1,12 +1,25 @@
 import torch
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification, BertModel, BertTokenizerFast
+from ml.embeddings.bert_pipeline import BERTClassifier
+import os
+import joblib
 
 # Load the saved model and tokenizer
-MODEL_PATH = "saved_model"
-TOKENIZER_PATH = "saved_model/tokenizer"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_DIR = os.path.join(BASE_DIR, "saved_model")
+MODEL_BIN = os.path.join(MODEL_DIR, "pytorch_model.bin")
+TOKENIZER_DIR = os.path.join("tokenizer")
+MLB_PATH = os.path.join(MODEL_DIR, "label_binarizer.pk1")
 
-tokenizer = BertTokenizer.from_pretrained(TOKENIZER_PATH)
-model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
+tokenizer = BertTokenizerFast.from_pretrained(TOKENIZER_DIR)
+mlb = joblib.load(MLB_PATH)
+num_labels = len(mlb.classes_)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+bert = BertModel.from_pretrained("bert-base-uncased")
+model = BERTClassifier(bert, num_labels=num_labels).to(device)
+state = torch.load(MODEL_BIN, map_location=device)
+model.load_state_dict(state)
 model.eval()
 
 def predict_mood(text):
